50845b1e63182e0b0debe934d6a1b9dd86e6ec62
scerelli <hi@stefanocerelli.com>
2024-03-18 18:09:47 +0100

    fix: oneko error

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
